2cfaa86f0612c23aedbfd0eb1f3d31513e04063c
webcodes <dev@wbcod.com>
2022-09-09 01:38:33 +0200

    enhancements

--- resources/views/code.blade.php
+++ resources/views/code.blade.php
@@ -1,5 +1,6 @@
 @foreach (range(1, 100) as $number)
-    <div>
-        <span>{{\App\Models\FuzzBuzz::get_fizz_buzz_or_number($number)}}</span>
+    <?php $value = \App\Models\FuzzBuzz::get_fizz_buzz_or_number($number);?>
+    <div class="group-item">
+        <span class="{{$value}}">{{$value}}</span>
     </div>
 @endforeach
